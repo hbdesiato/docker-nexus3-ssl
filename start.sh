8ec67b1863045136a33ae63174a17d7b4aee9bb5
hbdesiato <hbdesiato@gmx.de>
2016-12-07 13:39:10 +0100

    initial import

---
+++ start.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+if [ ! -e "$NEXUS_DATA/etc/ssl/keystore.jks" ]; then
+	mkdir -p "$NEXUS_DATA/etc/ssl"
+	chmod go-rwx "$NEXUS_DATA/etc/ssl"
+	/opt/java/bin/keytool -keystore "$NEXUS_DATA/etc/ssl/keystore.jks" \
+	-alias jetty -genkey -keyalg RSA -sigalg SHA256withRSA \
+	-validity 1095 -storepass password -keypass password \
+	-dname "cn=$HOST"
+fi
+bin/nexus run
+
