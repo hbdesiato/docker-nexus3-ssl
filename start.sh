c99097e97b37c25b0e4dc2554a2c472c4de2f711
hbdesiato <hbdesiato@gmx.de>
2016-12-13 13:28:03 +0100

    added basic constraints extension to self signed certificate

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ if [ ! -e "$NEXUS_DATA/etc/ssl/keystore.jks" ]; then
 	/opt/java/bin/keytool -keystore "$NEXUS_DATA/etc/ssl/keystore.jks" \
 	-alias jetty -genkey -keyalg RSA -sigalg SHA256withRSA \
 	-validity 1095 -storepass password -keypass password \
+	-ext BC:true=ca:true \
 	-dname "cn=$HOST"
 fi
 bin/nexus run
-
